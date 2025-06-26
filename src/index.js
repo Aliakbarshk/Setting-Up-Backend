d8868c8826e289723d632aee31afda7390107f39
Aliakbarshk <aliakbarsk05@gmail.com>
2025-06-26 12:53:16 +0530

    Connected to MongoAtlas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv';
+
+
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js";  
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env',
+})
+
+
+
+connectDB()
+
+
+
+
+
