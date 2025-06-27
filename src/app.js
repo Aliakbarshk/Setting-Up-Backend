bb350a7e7100f703bc020ef2cc380fb966ff9066
Aliakbarshk <aliakbarsk05@gmail.com>
2025-06-27 11:17:57 +0530

    added Utils

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
 const app = express() // just to use express as App (Reffernce)
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}
+
+))
+
+app.use(express.json({limit: "16kb" })) // Parse incoming JSON data from client (like fetch or axios requests)
+// Sets max body size limit to 16kb
+
+
+app.use(express.urlencoded({extended:true , limit: "16kb"})) // Parse URL-encoded data (like from HTML form submissions)
+// extended: true => allows nested objects & special characters
+// Sets max body size limit to 16kb
+
+
+app.use(express.static("public"))
+// Serve static files (HTML, CSS, JS, images, etc.) from the "public" folder
+// Example: "/style.css" will load from "public/style.css"
+app.use(cookieParser())
+
 
 
 export {app}
