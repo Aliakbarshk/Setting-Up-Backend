c0334f86e5bfa57e0e31c719bee2abd29e6e5851
Aliakbarshk <aliakbarsk05@gmail.com>
2025-06-27 10:19:28 +0530

    Added error handling in express if Db is not connecting it will throw error

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import express from "express"
+const app = express() // just to use express as App (Reffernce)
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -11,9 +11,25 @@ dotenv.config({
     path: './env',
 })
 
+const Port = process.env.PORT || 3000;
 
 
 connectDB()
+.then(()=>{
+    app.listen(Port, (req , res) => {
+        console.log(`Server is runnig at http://localhost:${Port}`)
+        res.send("<h1 style ='color:green'>Everything is working fine Database and Server</h1>");
+        console.dir(req.params.name);
+    })
+    app.on("error something went wrong :/ " , (err) =>{
+        console.log(`Error ${err}`)
+        res.send("Error Database Aint connecting");
+        
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed")
+})
 
 
 
