edcd5a3a2428f9db166fba6e8a3a609e7a2311cf
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-04 18:28:46 +0530

    Added Token function

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   username: {
@@ -43,6 +45,17 @@ refreshToken: {
 
 },{timestamps:true});
 
+userSchema.pre("save",async function(next) {
+  if(this.isModified("password")) // isModified jo hein ye mongoose ka ek function hai
 
+  this.password = await bcrypt.hash(this.password,10)
+  next()
+
+
+} )
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+   return await bcrypt.compare(password,this.password)
+}
 
 export const User = mongoose.model("User",userSchema)
