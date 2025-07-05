8bbd6507a1174e374be276364af5ba8c40a18805
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-06 05:28:19 +0530

    added Cloudinary file

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -1,6 +1,28 @@
 import {v2 as cloudinary} from "cloudinary"
 
-v2.uploader.upload
+ // Configuration
+ cloudinary.config({
+   cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
+   api_key: process.env.CLOUDNARY_API_KEY,
+   api_secret: process.env.CLOUDNARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+ });
 
 
 //fs File system
+
+const uploadCloudninary = async(localFilePath)=>{
+try{
+  if(!localFilePath) return null
+    const reponse = await cloudnary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    })
+  
+
+}
+catch(error){
+  
+}
+
+
+}
+//7:30:54
