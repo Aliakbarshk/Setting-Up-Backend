ff14baeca61e1a9197c290f6fdf4d0dc93c5f17e
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-10 11:04:25 +0530

    fixing controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,13 @@
-import { asyncHandler } from "../utils/AsyncHandler";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+
+try{
 
 const registerUser = asyncHandler(async (req , res) => {
-    res.status(200).json({
+   return res.status(200).json({
         message:"ok"
     })
-})
+   
+})}
+catch(err){
+    console.log(err,"Not working")
+}
